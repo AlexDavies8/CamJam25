3723702067344f7702c2f7a70870ff66c7030d2a
WillChit <wc376@cam.ac.uk>
2025-02-09 14:28:36 +0000

    added deformable sprite to player softbody

--- Assets/Scenes/Softbody.cs
+++ Assets/Scenes/Softbody.cs
@@ -14,6 +14,7 @@ public class Spring
     public float spring_coefficient;
     public float damping_coefficient; // big number => more damping
     public float min_length;
+    public bool anchor = false;
 
     public Spring(int u, int v, float rest_length, float spring_coefficient, float damping_coefficient)
     {
@@ -22,7 +23,6 @@ public class Spring
         this.rest_length = rest_length;
         this.spring_coefficient = spring_coefficient;
         this.damping_coefficient = damping_coefficient;
-        this.min_length = 0.1f;
     }
 }
 
@@ -30,11 +30,13 @@ public class Point
 {
     public Vector2 position;
     public Vector2 velocity;
+    public bool anchor;
 
-    public Point(Vector2 position)
+    public Point(Vector2 position, bool anchor)
     {
         this.position = position;
         this.velocity = new Vector2(0, 0);
+        this.anchor = anchor;
     }
 }
 
@@ -42,14 +44,27 @@ public class Softbody : MonoBehaviour
 {
     public List<Point> points = new List<Point>();
     public List<Spring> springs = new List<Spring>();
-    public void AddPoint(float x, float y)
+    public List<Vector2> net_force = new List<Vector2>();
+    public List<int> points_to_draw = new List<int>();
+    public void AddPoint(float x, float y, bool anchor)
     {
-        points.Add(new Point(new Vector2(x, y)));
+        points.Add(new Point(new Vector2(x, y), anchor));
+        net_force.Add(Vector2.zero);
     }
 
-    public void AddSpring(int u, int v)
+    public void AddPoint(Vector2 v, bool anchor)
     {
-        springs.Add(new Spring(u, v, (points[u].position - points[v].position).magnitude, 5f, 0.2f));
+        AddPoint(v.x, v.y, anchor);
+    }
+
+    public void AddPointToDraw(int x)
+    {
+        points_to_draw.Add(x);
+    }
+
+    public void AddSpring(int u, int v, float spring_coefficient)
+    {
+        springs.Add(new Spring(u, v, (points[u].position - points[v].position).magnitude, spring_coefficient, 0.3f));
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -68,87 +83,84 @@ public class Softbody : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        for (int _ = 0; _ < 4; _++)
+
+    }
+
+    public void Physics()
+    {
+        //apply physics
+        float dt = Time.fixedDeltaTime;
+        int n = points.Count;
+        foreach (Spring s in springs)
         {
-            //apply physics
-            float dt = Time.fixedDeltaTime;
-            int n = points.Count;
-            List<Vector2> net_force = new List<Vector2>();
-            for (int i = 0; i < n; i++)
+            int u = s.u;
+            int v = s.v;
+
+            Vector2 d = points[v].position - points[u].position;
+            float difference = d.magnitude - s.rest_length;
+            float force = difference * s.spring_coefficient;
+            d = d.normalized;
+
+            net_force[u] += d * force - Vector2.Dot(d, points[u].velocity) * d * s.damping_coefficient;
+            net_force[v] -= d * force - Vector2.Dot(d, points[v].velocity) * (-d) * s.damping_coefficient;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (points[i].anchor)
+                continue;
+            points[i].velocity += net_force[i] * dt;
+            points[i].position += points[i].velocity * dt;
+            net_force[i] = Vector2.zero;
+        }
+
+        //draw
+        LineRenderer l = this.GetComponent<LineRenderer>();
+        SpriteShapeController ssc = this.GetComponent<SpriteShapeController>();
+
+        List<int> ord = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            ord.Add(i);
+        }
+
+        Vector2 center = Vector2.zero;
+        for (int i = 0; i < n; i++)
+        {
+            center += points[i].position;
+        }
+        center /= n;
+
+        ord.Sort((int x, int y) => Compare(points[x].position, points[y].position, center));
+        List<int> ord_inv = new List<int>();
+        for (int i = 0; i < n; i++)
+            ord_inv.Add(0);
+        for (int i = 0; i < n; i++)
+        {
+            ord_inv[ord[i]] = i;
+        }
+
+        ssc.spline.Clear();
+
+        n = points_to_draw.Count;
+        int prev = -10000;
+        l.positionCount = n + 1;
+        foreach (int i in points_to_draw)
+        {
+            l.SetPosition(i, points[i].position);
+
+            int distance = ord_inv[i] - prev;
+            if (distance < -n / 2)
+                distance = n;
+            if (distance > 0)
             {
-                net_force.Add(Vector2.zero);
+                ssc.spline.InsertPointAt(ssc.spline.GetPointCount(), points[i].position);
+                prev = ord_inv[i];
             }
-            foreach (Spring s in springs)
-            {
-                int u = s.u;
-                int v = s.v;
-
-                Vector2 d = points[v].position - points[u].position;
-                float difference = d.magnitude - s.rest_length;
-                float force = difference * s.spring_coefficient;
-                d = d.normalized;
-
-                float damping_coefficient = s.damping_coefficient;
-                if (d.magnitude < s.min_length)
-                    damping_coefficient = 1f;
-
-                net_force[u] += d * force - Vector2.Dot(d, points[u].velocity) * d * s.damping_coefficient;
-                net_force[v] -= d * force - Vector2.Dot(d, points[v].velocity) * (-d) * s.damping_coefficient;
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                points[i].velocity += net_force[i] * dt;
-                points[i].position += points[i].velocity * dt;
-            }
-
-            //draw
-            LineRenderer l = this.GetComponent<LineRenderer>();
-            SpriteShapeController ssc = this.GetComponent<SpriteShapeController>();
-
-            List<int> ord = new List<int>();
-            for (int i = 0; i < n; i++)
-            {
-                ord.Add(i);
-            }
-
-            Vector2 center = Vector2.zero;
-            for (int i = 0; i < n; i++)
-            {
-                center += points[i].position;
-            }
-            center /= n;
-
-            ord.Sort((int x, int y) => Compare(points[x].position, points[y].position, center));
-            List<int> ord_inv = new List<int>();
-            for (int i = 0; i < n; i++)
-                ord_inv.Add(0);
-            for (int i = 0; i < n; i++)
-            {
-                ord_inv[ord[i]] = i;
-            }
-
-            ssc.spline.Clear();
-
-            int prev = -10000;
-            l.positionCount = n + 1;
-            for (int i = 0; i < n; i++)
-            {
-                l.SetPosition(i, points[i].position);
-
-                int distance = ord_inv[i] - prev;
-                if (distance < -n / 2)
-                    distance = n;
-                if (distance > 0)
-                {
-                    ssc.spline.InsertPointAt(ssc.spline.GetPointCount(), points[i].position);
-                    prev = ord_inv[i];
-                }
-            }
-            if (n > 0)
-            {
-                l.SetPosition(n, points[0].position);
-            }
+        }
+        if (n > 0)
+        {
+            l.SetPosition(n, points[0].position);
         }
     }
 }
